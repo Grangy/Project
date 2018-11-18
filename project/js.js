2fe4950a92e189463319719a1a4d0c0f5b3d4c8a
Grangy <grangy@live.ru>
2018-11-18 13:05:14 +0300

    commit 1

---
+++ project/js.js
@@ -0,0 +1,16 @@
+'use strict'
+// let person = {
+//     name: "John",
+//     age: 15,
+//     isMarried: false
+// };
+// console.log(person.age); //Выборка из функции
+
+
+// let arr = ['Max','Anton','Misha'];
+console.log(arr[1]); // Выборка из массива
+
+let itsCheked = true,
+    itsClose = false;
+
+    console.log(itsCheked || !itsClose);
